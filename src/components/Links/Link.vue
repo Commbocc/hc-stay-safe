e86f333a315655f943f2a7921f5ab91234e25961
Kevin McCarthy <mccarthyk@hcflgov.net>
2019-08-29 11:26:39 -0400

    init vue-cli version

---
+++ src/components/Links/Link.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    functional: true,
+    props: ['link'],
+    render: function (createElement, context) {
+      return createElement('a', Object.assign(context.data, context.props.link.data), context.props.link.children)
+    }
+}
+</script>
+
+<style media="screen" scoped>
+  a[target="_blank"]:after {
+    content: "\29C9";
+    text-align: right;
+    float:right;
+  }
+</style>
